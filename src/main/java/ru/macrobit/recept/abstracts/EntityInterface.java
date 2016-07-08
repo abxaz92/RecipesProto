27ce6ef7d53bd89c31a700996b7c7d94d61c6d8f
david <chaava01@gmail.com>
2016-07-08 20:26:11 +0300

    init

---
+++ src/main/java/ru/macrobit/recept/abstracts/EntityInterface.java
@@ -0,0 +1,12 @@
+package ru.macrobit.recept.abstracts;
+
+/**
+ * Интерфейс сущности, от которой все остальные сущности наследуются
+ * чтобы каркас работы с БД мог с ними работать
+ */
+public interface EntityInterface {
+
+    Long getId();
+
+    void setId(Long id);
+}
---
+++ src/main/java/ru/macrobit/recept/commons/ExceptionFactory.java
@@ -0,0 +1,36 @@
+package ru.macrobit.recept.commons;
+
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+/**
+ * Класс которые предоставляет интерфейс для обработки ошибок
+ */
+public class ExceptionFactory {
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ExceptionFactory.class);
+
+
+    public static WebApplicationException exception(Response.Status status) {
+        return new WebApplicationException(Response.status(status)
+                .entity(new Error(status.getReasonPhrase())).build());
+    }
+
+    public static WebApplicationException exception(Response.Status status, Throwable cause) {
+        LOG.warn("{} : {}", status.getStatusCode(), cause.toString());
+        String resp = cause.getMessage() == null ? status.getReasonPhrase() : cause.getMessage();
+        return new WebApplicationException(Response.status(status).entity(new Error(resp)).build());
+    }
+
+    public static WebApplicationException exception(Response.Status status, String cause) {
+        LOG.warn("{} : {}", status.getStatusCode(), cause);
+        return new WebApplicationException(Response.status(status)
+                .entity(new Error(cause)).build());
+    }
+
+    public static Response getResponse(Response.Status status, String cause) {
+        return Response.status(status)
+                .entity(new Error(cause)).build();
+    }
+}
