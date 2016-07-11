c2ee13bfd0e94327b8acac097ef226397bcbfdb3
david <chaava01@gmail.com>
2016-07-11 12:57:16 +0300

    check json query

--- src/main/java/ru/macrobit/recept/controller/LpuController.java
+++ src/main/java/ru/macrobit/recept/controller/LpuController.java
@@ -1,14 +1,10 @@
 package ru.macrobit.recept.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.sun.xml.internal.ws.api.message.Attachment;
-import jdk.nashorn.internal.parser.JSONParser;
-import org.hibernate.criterion.Order;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.macrobit.recept.commons.Recept;
 import ru.macrobit.recept.pojo.Lpu;
 import ru.macrobit.recept.service.LpuService;
 
@@ -16,7 +12,6 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Created by david on 7/8/16
@@ -41,7 +36,7 @@ public class LpuController {
                              @QueryParam("count") String count, @QueryParam("skip") Integer skip,
                              @QueryParam("limit") Integer limit, @QueryParam("sort") String sortProperties,
                              @QueryParam("direction") String sortDirection) throws IOException {
-        return lpuService.findAll(new JSONObject(jsonQuery), skip, limit, sortProperties, sortDirection);
+        return lpuService.findAll(jsonQuery == null ? null : new JSONObject(jsonQuery), skip, limit, sortProperties, sortDirection);
     }
 
     @POST
