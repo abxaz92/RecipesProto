6155e9509454b1d3070f18c9d1dc030fd319498f
david <chaava01@gmail.com>
2016-09-15 21:56:18 +0300

    change commands

--- sql/commands.sql
+++ sql/commands.sql
@@ -1,4 +1,4 @@
- alter table users add constraint lpu_fk foreign key (lpu_id) references lpu(id);
+alter table users add constraint lpu_fk foreign key (lpu_id) references lpu(id);
 
 grant ALL privileges on database recept to recept;
 grant ALL privileges on all tables in schema public to recept;
